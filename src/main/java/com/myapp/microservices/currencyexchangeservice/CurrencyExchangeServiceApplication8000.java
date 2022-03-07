565a8caefb39fa5bf05ab7852ec7ec17c0973c7c
Rabia <sdetcode@gmail.com>
2022-03-07 22:59:46 +0530

    spring cloud sleuth implementation

--- src/main/java/com/myapp/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication8000.java
+++ src/main/java/com/myapp/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication8000.java
@@ -1,8 +1,10 @@
 package com.myapp.microservices.currencyexchangeservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,12 @@ public class CurrencyExchangeServiceApplication8000 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication8000.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler()
+	{
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
